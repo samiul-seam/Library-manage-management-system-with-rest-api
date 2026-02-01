940cb0dba4691df2a6e45c54c8d06a592eef983a
samiul haque <mdsamiulhaque682@gmail.com>
2026-02-01 21:13:55 +0600

    Fix API root visibility for normal users

--- book/views.py
+++ book/views.py
@@ -157,7 +157,7 @@ class BookViewSet(ModelViewSet):
 
 class BookImageViewSet(ModelViewSet):
     serializer_class = BookImageSerializer
-    permission_classes = [IsLibrarainOrReadOnly]
+    permission_classes = [IsAdminUser]
 
     def get_queryset(self):
         return BookImages.objects.filter(book_id=self.kwargs.get('book_pk'))
